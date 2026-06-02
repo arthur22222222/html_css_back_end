7604d3e80a44239763bb0db3b86fdb06bdd917cb
arthur22222222 <146186994+arthur22222222@users.noreply.github.com>
2026-06-02 09:32:10 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.post("/cliente/cadastrar", (req, res) => {
   }
 
 })
+app.get("/clientes", (req,res) => {
+  try{
+    const clientes = JSON.parse(fs.readFileSync('bd.json','utf8'))
+    res.status(200).json(clientes)
+  } catch(error){
+    res.status(500).json({resposta: error.message})
+  }
+})
 
 app.listen(port, () => {
   console.log(`API executando na porta ${port}`);
